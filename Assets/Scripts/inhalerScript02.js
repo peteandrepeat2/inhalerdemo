330cb428b62d56036323e9305152a59a2476b026
Ben Coonley <bcoonley@gmail.com>
2017-04-12 13:29:58 -0400

    initial check-in

---
+++ Assets/Scripts/inhalerScript02.js
@@ -0,0 +1,17 @@
+﻿// Function called about 60 times per second
+function Update() {
+    // Get the rigidbody component
+    var r2d = GetComponent("Rigidbody2D");
+
+	 // Move the inhaler when an arrow key is pressed
+    if (Input.GetKey("right")) {
+        r2d.velocity.x = 10;
+       	transform.rotation.y = 180;                          //This will make the sprite switch direction depending on the direction
+       	}
+    else if (Input.GetKey("left")) {
+        r2d.velocity.x = -10;
+        transform.rotation.y = 0;                          //This will make the sprite switch direction depending on the direction
+        }
+    else
+        r2d.velocity.x = 0;
+ }
---
+++ Assets/Scripts/inhalerScript03.js
@@ -0,0 +1,41 @@
+﻿
+public var jumpAnimationName : String = "playerSpin";        //This should be the name of jump trigger in the animator.
+public var sprayAnimationName : String = "playerSpray";        //This should be the name of spray trigger in the animator.
+private var anim : Animator;                                    //DO NOT CHANGE THIS!
+
+
+
+function Start() {
+	anim = GetComponent(Animator);
+    }
+
+
+// Function called about 60 times per second
+function Update() {
+    // Get the rigidbody component
+    var r2d = GetComponent("Rigidbody2D");
+
+	 // Move the inhaler when an arrow key is pressed
+    if (Input.GetKey("right")) {
+        r2d.velocity.x = 10;
+       	transform.rotation.y = 180;                          //This will make the sprite switch direction depending on the direction
+       	}
+    else if (Input.GetKey("left")) {
+        r2d.velocity.x = -10;
+        transform.rotation.y = 0;                          //This will make the sprite switch direction depending on the direction
+        }
+	 else if( Input.GetKeyDown( KeyCode.Space ) ) {
+		anim.SetTrigger (sprayAnimationName);
+
+	}
+    else
+        r2d.velocity.x = 0;
+
+    if (Input.GetKeyDown("up")) {
+    	r2d.velocity.y = 5;
+		anim.SetTrigger (jumpAnimationName);
+		}
+
+ }
+
+     
